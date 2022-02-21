8debaef525799d66a64c5bcf4dca1f63b9001ddc
taikinoda <taiki.noda@theport.jp>
2022-02-21 23:13:54 +0900

    1-1

--- 1-1.sql
+++ 1-1.sql
@@ -6,5 +6,13 @@ SELECT key,
              WHEN z >= x AND x >= y THEN z) AS greatest
 FROM Greatests;
 
+SELECT key, MAX(col) AS greatest
+FROM (SELECT key, x AS col FROM Greatests
+      UNION ALL
+      SELECT key, y AS col FROM Greatests
+      UNION ALL
+      SELECT key, z AS col FROM Greatests) TMP
+GROUP BY key;
+
 -- 標準SQLではない
 SELECT key, GREATEST(GREATEST(x,y),z) AS greatest FROM Greatests;
